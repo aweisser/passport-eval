92ae57cbd1683a1d42ba4e2d46744d3ce423a77a
vagrant <vagrant@precise64.(none)>
2014-02-16 00:23:47 +0000

    added google auth

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ var users = [
 ];
 
 function findById(id, fn) {
-	var idx = id - 1;
-	if (users[idx]) {
-		fn(null, users[idx]);
-	} else {
-		fn(new Error('User ' + id + ' does not exist'));
+	for (var i = 0, len = users.length; i < len; i++) {
+		var user = users[i];
+		if (user.id === id) {
+			return fn(null, user);
+		}
 	}
+	return fn(null, null);
 }
 
 function findByUsername(username, fn) {
